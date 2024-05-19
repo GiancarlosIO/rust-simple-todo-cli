82595d4300a901ebe08e2beb97ba94a2f5c1a30b
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2024-05-19 12:07:28 -0500

    feat: mark the root error as a critical
    This must happen only when the other errors has been ignores because a critical error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     if let Err(err) = todo_cli_3::new_cli().and_then(todo_cli_3::run) {
-        eprintln!("{}", err);
+        eprintln!("Critical Error: {}", err);
         std::process::exit(1);
     }
 }
